30895b7d049fba5e59670f9f9a439f778ae7bab2
DanSeb1295 <daniel-sebastian95@hotmail.com>
2020-06-12 04:33:42 +0800

    newline bugfix

--- client/src/util/SongSearch.js
+++ client/src/util/SongSearch.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 const HOST = 'https://api.lyrics.ovh/v1'
 
 const songSearch = async (artist, title) => {
-  artist = artist.trim();
-  title = title.trim();
-  const endpoint = `${HOST}/${artist}/${title}`
+  const endpoint = `${HOST}/${artist.trim()}/${title.trim()}`
 
   return new Promise((resolve, reject) => {
     axios.get(endpoint)
