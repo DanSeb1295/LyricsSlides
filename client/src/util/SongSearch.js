6d7062d434e135f7696153a9b8e2a9868d261c0f
DanSeb1295 <daniel-sebastian95@hotmail.com>
2020-07-11 01:08:05 +0800

    searches metro lyrics & calls API on fail

--- client/src/util/SongSearch.js
+++ client/src/util/SongSearch.js
@@ -13,21 +13,31 @@ const songSearch = async (artist, title) => {
   }
 
   // 2. Check MetroLyrics
+  return await axios
+    .post('/api/searchMetro', { artist, title })
+    .then(res => { return res.data })
+    .catch(err => {
+      return searchAPI(artist, title);
+    })
 
   // 3. Check AZLyrics
 
-  // 4. Check Lyrics.ovh
+  // 4. Check https://lyrics.fandom.com/wiki
+}
+
+const searchAPI = (artist, title) => {
+  // 5. Check Lyrics.ovh
   const endpoint = `${HOST}/${artist.trim()}/${title.trim()}`
 
   return new Promise((resolve, reject) => {
     axios.get(endpoint)
         .then(res => { 
-          axios.post('/api/search', { artist, title, result: 'success' })
-            .then(apiRes => { resolve(res.data.lyrics) })
+          axios.post('/api/songs', { artist, title, result: 'success' })
+          resolve(res.data.lyrics)
         })
         .catch(err => { 
-          axios.post('/api/search', { artist, title, result: 'failed' })
-            .then(apiRes => { reject() })
+          axios.post('/api/songs', { artist, title, result: 'failed' })
+          reject()
         })
   })
 }
--- server.js
+++ server.js
@@ -1,41 +1,75 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient
+const cheerio = require('cheerio');
+const request = require('request')
 
 const app = express();
 app.use(express.json({limit: '50mb', extended: true})); // support json encoded bodies
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 /* -------------------- Connect MongoDB -------------------- */
-const mongoPW = process.env.MONGO_PW
-const mongoUser = process.env.MONGO_USER
-const url = `mongodb+srv://${mongoUser}:${mongoPW}@lyricsslides.xlmpb.mongodb.net/LyricsSlides?retryWrites=true&w=majority`
+// const mongoPW = process.env.MONGO_PW
+// const mongoUser = process.env.MONGO_USER
+// const url = `mongodb+srv://${mongoUser}:${mongoPW}@lyricsslides.xlmpb.mongodb.net/LyricsSlides?retryWrites=true&w=majority`
 
-/* -------------------- APIs -------------------- */
-app.post('/api/search', async (req, res) => {
-  MongoClient
-  .connect(url, (err, db) => {
-    if (err) {
-      console.log(err);
-      return
-    };
-    console.log("MongoDB Connected...")
+// /* -------------------- APIs -------------------- */
+// app.post('/api/songs', async (req, res) => {
+//   MongoClient
+//   .connect(url, (err, db) => {
+//     if (err) throw err;
+//     console.log("MongoDB Connected...")
 
-    const { body: { artist, title, result }} = req;
-    const dbo = db.db('LyricsSlides');
-    dbo.collection("SearchedSongs")
-      .insertOne({ artist, title, result }, (err, dbRes) => {
-        if (err) {
-          console.log(err);
-          return
-        };
+//     const { body: { artist, title, result }} = req;
+//     const dbo = db.db('LyricsSlides');
+//     dbo.collection("SearchedSongs")
+//       .insertOne({ artist, title, result }, (err, dbRes) => {
+//         if (err) {
+//           console.log(err);
+//           return
+//         };
 
-        const { insertedId } = dbRes;
-        res.send({ insertedId });
-      });
+//         const { insertedId } = dbRes;
+//         res.send({ insertedId });
+//       });
 
-    db.close();
-  })
+//     db.close();
+//   })
+// })
+
+app.post('/api/searchMetro', async (req, res) => {
+  const lyrics = await searchMetro(req, res)
+  if (!lyrics.length) res.status(400).send();
+  res.send(lyrics)
 })
+
+const searchMetro = (req, res) => {
+  return new Promise((resolve, reject) => {
+    const { body: { artist, title }} = req;
+    const url = `https://www.metrolyrics.com/${title.trim().replace('(', '').replace(')', '').split(' ').join('-')}-lyrics-${artist.trim().replace('(', '').replace(')', '').split(' ').join('-')}.html`;
+    request(
+      url,
+      (err, resp, body) => {
+        if (err) throw err;
+        let paras = []
+
+        const $ = cheerio.load(body);
+        $('#lyrics-body-text p.verse ').each((i, para) => {
+          $(this).find('br').removeAttr('clear');
+          let lines = para.children
+            .slice(1, para.children.length)
+            .filter(x => x.data !== undefined
+              && !x.data.toLowerCase().includes('verse')
+              && !x.data.toLowerCase().includes('chorus')
+              && !x.data.toLowerCase().includes('bridge'))
+            .map(lineObj => lineObj.data);
+          
+          paras.push(lines.join() + '\n')
+        })
+
+        return resolve(paras.join(''))
+      })
+  })
+}
 
 /* -------------------- Run Server -------------------- */
 const port = process.env.PORT || 5000;
